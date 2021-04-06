4269e240f032b1b2b36845f1cfdac1a3bc4fa19b
Yuta Koma <yutakoma54@gmail.com>
2021-04-06 18:13:26 +0900

    コミット

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function Home({ blog }) {
 				<title>My page title</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<meta name="description" content="ポートフォリオサイトです" />
-				<meta property="og:title" content="My page title" key="title" />
 			</Head>
 			<header>header</header>
 			<ul>
