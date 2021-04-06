73e9dce7b62c4c2311efa1be936629e7be3011a6
Yuta Koma <yutakoma54@gmail.com>
2021-04-06 18:34:16 +0900

    Headコンポーネント追加

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 // pages/index.js
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Home({ blog }) {
 	console.log(blog)
 	return (
 		<div>
+			<Head>
+				<title>My page title</title>
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			</Head>
 			<header>header</header>
 			<ul>
 				{blog.map(blog => (
